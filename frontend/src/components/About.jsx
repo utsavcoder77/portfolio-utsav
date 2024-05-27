2cb6d90c203215a7f82ca64cca22842d1c97b1fc
utsavcoder77 <baralutsav89@gmail.com>
2024-05-27 17:51:26 +1000

    added navbar and hero section in the project

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,11 @@
+
+
+function About() {
+    return (
+        <div className="border-b border-neutral-900 pb-4  w-full max-w-screen-xl">
+            <h1>About</h1>
+        </div>
+    )
+}
+
+export default About;
---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import { HERO_CONTENT } from "../constants/index.js";
+import ProfilePic from "../assets/utsavProfile.jpeg"
+
+function Hero() {
+    return (
+        <div className="border-b border-neutral-900 pb-4 w-screen lg:mb-35">
+            <div className="flex w-full max-w-8xl flex-wrap mx-5">
+                <div className="w-full lg:w-1/2">
+                    <div className="flex flex-col items-center lg:items-start">
+                        <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Utsav Baral</h1>
+                        <span className="bg-gradient-to-r from-sky-400 via-rose-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Full Stack Developer</span>
+                        <p className="m-2 leading-8 max-w-l py-6 font-light tracking-tighter">{HERO_CONTENT}</p>
+                    </div>
+                </div>
+                <div className="w-full lg:w-1/2 lg:p-8">
+                    <div className="flex justify-center">
+                        <img className="w-[400px] rounded-3xl" src={ProfilePic} alt="Utsav Baral" />
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Hero;
