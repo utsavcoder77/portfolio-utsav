d3022f448e360e65aaa284f4e18bffa3b21fea00
utsavcoder77 <baralutsav89@gmail.com>
2024-06-18 17:15:31 +1000

    added hero section

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,13 +3,13 @@ import ProfilePic from "../assets/utsavProfile.jpeg"
 
 function Hero() {
     return (
-        <div className="border-b border-neutral-900 pb-4 w-screen lg:mb-35">
-            <div className="flex w-full max-w-8xl flex-wrap mx-5">
+        <div className="border-b border-neutral-900 pb-4 w-full lg:mb-35">
+            <div className="flex w-full max-w-screen-2xl flex-wrap mx-10">
                 <div className="w-full lg:w-1/2">
                     <div className="flex flex-col items-center lg:items-start">
                         <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Utsav Baral</h1>
                         <span className="bg-gradient-to-r from-sky-400 via-rose-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Full Stack Developer</span>
-                        <p className="m-2 leading-8 max-w-l py-6 font-light tracking-tighter">{HERO_CONTENT}</p>
+                        <p className="m-2 leading-10 max-w-l py-6 font-light tracking-tighter">{HERO_CONTENT}</p>
                     </div>
                 </div>
                 <div className="w-full lg:w-1/2 lg:p-8">
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,9 +8,9 @@ function Navbar() {
 
     return (
 
-        <nav className='mb-20 flex items-center justify-between py-6'>
+        <nav className='mb-20 w-full flex items-center justify-between py-6'>
             <div className='flex flex-shrink-0 items-center'>
-                <img className="mx-3 w-24 brightness-75" src={logo} alt="logo" />
+                <img className="w-24 brightness-75" src={logo} alt="logo" />
             </div>
             <div className="m-8 flex items-center justify-center gap-4 text-2xl">
                 <FaLinkedin />
