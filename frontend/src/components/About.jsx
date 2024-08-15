d04be37209f8f123105a662b52120f2fc77424ce
utsavcoder77 <baralutsav89@gmail.com>
2024-08-15 19:06:49 +1000

    updated about page

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -5,7 +5,7 @@ function About() {
     return (
         <div className="flex flex-col-reverse md:flex-row gap-6">
             <figure className="self-end">
-                <img src={picture} className="h-[400px]"></img>
+                <img src={picture} className="h-[400px] brightness-50 rounded-3xl"></img>
             </figure>
             <div className="border-b border-neutral-900 pb-4  w-full max-w-screen-xl mt-32 flex flex-col gap-14">
                 < h1 className="inline-block bg-gradient-to-r from-cyan-600 via-orange-800 to-indigo-400 bg-clip-text text-5xl text-transparent">About my Skills on full-stack development</h1>
