f30ffb08f08b555c7e3857d611f183f83010958b
utsavcoder77 <160719155+utsavcoder77@users.noreply.github.com>
2024-12-21 13:37:03 +1100

    updated portfolio

---
+++ frontend/src/components/AboutHome.jsx
@@ -0,0 +1,23 @@
+import { ABOUT_TEXT } from "../constants/index";
+import picture from "../assets/images/utsav_about.png";
+
+function AboutHome() {
+    return (
+        <>
+            < h1 className="inline-block bg-gradient-to-r from-cyan-600 via-orange-800 to-indigo-400 bg-clip-text text-5xl text-transparent mt-20">About my Skills on full-stack development</h1>
+            <div className="flex flex-col-reverse md:flex-row gap-10 mt-20">
+                <figure className="self-end basis-1/2">
+                    <img src={picture} className="h-[250px] brightness-50 rounded-3xl grayscale-0 hover:grayscale transition-all hover:scale-[98%]"></img>
+                </figure>
+                <div className="basis-1/2">
+                    
+                    <p>{ABOUT_TEXT}</p>
+                </div>
+
+            </div>
+        </>
+        
+    )
+}
+
+export default AboutHome;
---
+++ frontend/src/components/ExperiencePage.jsx
@@ -0,0 +1,12 @@
+import { EXPERIENCES } from "../constants";
+
+function ExperiencePage() {
+    return (
+        <div className="border-b border-neutral-900 pb-4  w-full max-w-screen-xl  flex flex-col gap-14">
+            <h1 className="inline-block bg-gradient-to-r from-sky-400 via-orange-800 to-cyan-500 bg-clip-text text-5xl text-transparent">Experience</h1>
+            <p>{EXPERIENCES}</p>
+        </div>
+    )
+}
+
+export default ExperiencePage;
