7c1ba89c42bd9a9c135a0ab948248885d3a46e43
utsavcoder77 <160719155+utsavcoder77@users.noreply.github.com>
2025-03-17 21:31:12 +1100

    updated contact form

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -32,17 +32,13 @@ function Contact() {
         setbackendError(error.messages.join(", "));
       } else if (success) {
         console.log(success.message);
+        console.log("Form submission successful!");
         setSubmittingForm(true);
       }
     } catch (err) {
       setbackendError(err.message);
     }
 
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve();
-      }, 1000);
-    });
 
   }
   return (
@@ -63,128 +59,134 @@ function Contact() {
         }
 
         {
-          submittingForm ? (<ContactFormSubmitted />) : (
-            <form onSubmit={handleSubmit(submitData)} className="space-y-6">
+          submittingForm ?
+            <>
+              
+              <ContactFormSubmitted /></>
+         
+            :
+            (
+              <form onSubmit={handleSubmit(submitData)} className="space-y-6">
 
-              <div className="w-full">
-                <h2 className="text-2xl font-semibold leading-9 tracking-tight text-slate-400">
-                  Please fillup the contact form below
-                </h2>
-              </div>
+                <div className="w-full">
+                  <h2 className="text-2xl font-semibold leading-9 tracking-tight text-slate-400">
+                    Please fillup the contact form below
+                  </h2>
+                </div>
 
-              <div className="grid grid-cols-2 gap-4">
-                <div>
-                  <label
-                    htmlFor="fName"
-                    className="block text-sm font-medium leading-6 text-slate-200"
-                  >
-                    First Name
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="fName"
-                      {...register("fName")}
-                      type="text"
+                <div className="grid grid-cols-2 gap-4">
+                  <div>
+                    <label
+                      htmlFor="fName"
+                      className="block text-sm font-medium leading-6 text-slate-200"
+                    >
+                      First Name
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="fName"
+                        {...register("fName")}
+                        type="text"
 
-                      className="block w-full rounded-md border-0 py-1.5 px-2 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-transparent"
-                    />
+                        className="block w-full rounded-md border-0 py-1.5 px-2 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-transparent"
+                      />
+                    </div>
+
+                  </div>
+
+                  <div>
+                    <label
+                      htmlFor="lName"
+                      className="block text-sm font-medium leading-6 text-slate-200"
+                    >
+                      Last Name
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="lName"
+                        {...register("lName")}
+                        type="text"
+
+                        className="block w-full rounded-md border-0 py-1.5 px-2 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-transparent"
+                      />
+                    </div>
+
+                  </div>
+                </div>
+
+                <div className="grid grid-cols-2 gap-4">
+                  <div>
+                    <label
+                      htmlFor="Mobile"
+                      className="block text-sm font-medium leading-6 text-slate-200"
+                    >
+                      Mobile Number*
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="mobile"
+                        {...register("mobile")}
+                        type="text"
+
+                        className="block w-full rounded-md border-0 py-1.5 px-2 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-transparent"
+                      />
+                    </div>
+                    {errors.mobile && (
+                      <span className="text-xs text-red-500">
+                        {errors.mobile.message}
+                      </span>
+                    )}
+
+                  </div>
+
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium leading-6 text-slate-200"
+                    >
+                      Email*
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="email"
+                        {...register("email")}
+                        type="email"
+
+                        className="block w-full rounded-md border-0 py-1.5 px-2 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-transparent"
+                      />
+                    </div>
+                    {errors.email && (
+                      <span className="text-xs text-red-500">
+                        {errors.email.message}
+                      </span>
+                    )}
                   </div>
 
                 </div>
-
                 <div>
                   <label
-                    htmlFor="lName"
+                    htmlFor="message"
                     className="block text-sm font-medium leading-6 text-slate-200"
                   >
-                    Last Name
+                    Message
                   </label>
                   <div className="mt-2">
-                    <input
-                      id="lName"
-                      {...register("lName")}
-                      type="text"
-
+                    <textarea rows={10}
+                      {...register("message")}
                       className="block w-full rounded-md border-0 py-1.5 px-2 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-transparent"
                     />
                   </div>
-
                 </div>
-              </div>
-
-              <div className="grid grid-cols-2 gap-4">
-                <div>
-                  <label
-                    htmlFor="Mobile"
-                    className="block text-sm font-medium leading-6 text-slate-200"
-                  >
-                    Mobile Number*
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="mobile"
-                      {...register("mobile")}
-                      type="text"
-
-                      className="block w-full rounded-md border-0 py-1.5 px-2 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-transparent"
-                    />
-                  </div>
-                  {errors.mobile && (
-                    <span className="text-xs text-red-500">
-                      {errors.mobile.message}
-                    </span>
-                  )}
-
-                </div>
-
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block text-sm font-medium leading-6 text-slate-200"
-                  >
-                    Email*
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="email"
-                      {...register("email")}
-                      type="email"
-
-                      className="block w-full rounded-md border-0 py-1.5 px-2 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-transparent"
-                    />
-                  </div>
-                  {errors.email && (
-                    <span className="text-xs text-red-500">
-                      {errors.email.message}
-                    </span>
-                  )}
-                </div>
-
-              </div>
-              <div>
-                <label
-                  htmlFor="message"
-                  className="block text-sm font-medium leading-6 text-slate-200"
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-black px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  disabled={isSubmitting}
                 >
-                  Message
-                </label>
-                <div className="mt-2">
-                  <textarea rows={10}
-                    {...register("message")}
-                    className="block w-full rounded-md border-0 py-1.5 px-2 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 bg-transparent"
-                  />
-                </div>
-              </div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-black px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                disabled={isSubmitting}
-              >
-                {isSubmitting ? <><FiLoader className="animate-spin" /></> : "Submit"}
-              </button>
+                  {isSubmitting ? <><FiLoader className="animate-spin" /></> : "Submit"}
+                </button>
 
-            </form>
-          )
+              </form>
+            )
         }
 
       </div>
