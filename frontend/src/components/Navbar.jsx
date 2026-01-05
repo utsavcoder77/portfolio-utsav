1e1e34e034a8db6161e74ac12b06bb2bb7b6f1f0
utsavcoder77 <160719155+utsavcoder77@users.noreply.github.com>
2026-01-06 10:03:22 +1100

    updated logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,58 +1,47 @@
-import logo from "../assets/images/logo.png";
+import logo from "../assets/images/transparent_logo.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { motion } from "framer-motion";
 
-
 function Navbar() {
+  return (
+    <nav className="w-full flex items-center justify-between py-6 h-40">
+      <motion.div
+        initial={{ x: -80 }}
+        animate={{ x: 0 }}
+        transition={{
+          duration: 1,
+        }}
+        className="flex flex-shrink-0 items-center"
+      >
+        <img className="max-w-64 mix-blend-screen" src={logo} alt="logo" />
+      </motion.div>
+      <motion.div
+        initial={{ x: 80 }}
+        animate={{ x: 0 }}
+        transition={{
+          duration: 1,
+        }}
+        className="m-8 flex items-center justify-center gap-4 text-2xl"
+      >
+        <a href="https://www.linkedin.com/in/utsav-baral/" target="blank">
+          <FaLinkedin />
+        </a>
 
-    return (
-
-        <nav className='w-full flex items-center justify-between py-6'>
-            <motion.div
-                initial={{ x: -80 }}
-                animate={{ x: 0 }}
-                transition={
-                    {
-                        duration: 1,
-
-                    }
-                }
-
-                className='flex flex-shrink-0 items-center'>
-                <img className="w-32 mix-blend-screen" src={logo} alt="logo" />
-            </motion.div>
-            <motion.div
-                initial={{ x: 80 }}
-                animate={{ x: 0 }}
-                transition={
-                    {
-                        duration: 1,
-                    
-                    }
-                }
-
-                className="m-8 flex items-center justify-center gap-4 text-2xl">
-                <a href="https://www.linkedin.com/in/utsav-baral/" target="blank">
-                    <FaLinkedin />
-                </a>
-
-                <a href="https://github.com/utsavcoder77">
-                    <FaGithub />
-                </a>
-                <a href="https://www.facebook.com/profile.php?id=100008693744003">
-                    <FaFacebook />
-                </a>
-                <a href="https://x.com/Baral_U47">
-                    <FaTwitter />
-                </a>
-
-            </motion.div>
-
-        </nav>
-    )
+        <a href="https://github.com/utsavcoder77">
+          <FaGithub />
+        </a>
+        <a href="https://www.facebook.com/profile.php?id=100008693744003">
+          <FaFacebook />
+        </a>
+        <a href="https://x.com/Baral_U47">
+          <FaTwitter />
+        </a>
+      </motion.div>
+    </nav>
+  );
 }
 
 export default Navbar;
