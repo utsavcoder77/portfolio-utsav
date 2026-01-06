4a5da65074b89f688ce7dc0d0209e9bc6de54ea9
utsavcoder77 <160719155+utsavcoder77@users.noreply.github.com>
2026-01-06 13:07:27 +1100

    removed backend

--- backend/routes/contact.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-router.post("/", function (req, res, next) {
-  res.status(200).json({ success: true, message: "Message sent!" });
-});
-
-export default router;
