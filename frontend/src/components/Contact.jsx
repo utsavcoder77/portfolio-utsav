32d988d567db850539f7d0cc4543b8e1a8d5f94b
utsavcoder77 <160719155+utsavcoder77@users.noreply.github.com>
2026-01-11 11:47:13 +1100

    changed css for hover in submit button of contact form

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -171,7 +171,7 @@ function Contact() {
             </div>
             <button
               type="submit"
-              className="flex w-full justify-center rounded-md bg-black px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="flex w-full justify-center rounded-md bg-black px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-slate-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               disabled={isSubmitting}
             >
               {isSubmitting ? (
