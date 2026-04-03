de6af36cc97cb60aee8a6ea5862f7d4b25cc04a1
utsavcoder77 <160719155+utsavcoder77@users.noreply.github.com>
2026-04-03 13:26:15 +1100

    updated backend

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,10 +1,6 @@
 import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from "../../generated/prisma/client.js"
+import { PrismaClient } from "@prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`
+const prisma = new PrismaClient();
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
-
-export { prisma }
+export { prisma };
