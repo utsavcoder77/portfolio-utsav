5d6cafe7ca91ce6ac07e91b52d6c7aca8731f53c
utsavcoder77 <160719155+utsavcoder77@users.noreply.github.com>
2026-04-03 14:47:42 +1100

    updated frontend

--- frontend/src/actions/contactAction.js
+++ frontend/src/actions/contactAction.js
@@ -1,13 +1,14 @@
 import { API_URL } from "../constants";
 async function contactAction(data) {
-    const url = `${API_URL}/contact`;
-    const respone = await fetch(url, {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-    });
-    return respone;
+  const url = `${API_URL}/contact`;
+
+  const respone = await fetch(url, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return respone;
 }
 export default contactAction;
